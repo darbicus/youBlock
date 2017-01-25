3dafc35f9e27ece8f1f8e44d3c58b1474b47415a
Darby Rathbone <darby.rathbone@gmail.com>
2017-01-24 18:07:58 -0800

    rewrite

--- ContentScript.js
+++ ContentScript.js
@@ -1,41 +1,65 @@
 (function() {
-    "use strict";
-    var names = [];
-    chrome.storage.sync.get("names", function(e) {
-        console.log(e);
-        names = e.names||[];
-    });
-    var listenForAllComments = function(e) {
-        if (e.target.querySelector) {
-            var allcomments = e.target.querySelector('#all-comments');
-            if (allcomments) {
-                allcomments.addEventListener('DOMNodeInserted', function(e) {
-                    var flag = e.target.querySelector('button[data-action="flag"]'),
-                        author = e.target.querySelector('.author'),
-                        authortext = author ? author.textContent.trim() : '';
-                    if (flag && authortext) {
-                        flag.addEventListener('click', function(f) {
-                            names.push(authortext);
-                            chrome.storage.sync.set({
-                                "names": names
-                            });
-                        });
-                    }
-                    if (authortext && names.some(function(f) {
-                            return f === authortext;
-                        })) {
-                        console.log("blocked " + authortext);
-                        e.target.style.display = 'none';
-                    }
-                });
-                removeEventListener("DOMNodeInserted", listenForAllComments);
+  "use strict";
+  var names = [];
+  chrome.storage.sync.get("names", function(e) {
+    console.log(e);
+    names = e.names || [];
+  });
+  var listenForAllComments = function(e) {
+    if (e.target.querySelector) {
+
+      var frame = document.getElementById("live-chat-iframe").contentDocument,
+        allcomments = frame.querySelector('#contents > yt-live-chat-renderer');
+      var blockbutton = frame.querySelector('ytd-menu-service-item-renderer');
+
+      if (allcomments) {
+        allcomments.addEventListener('DOMNodeInserted', function(e) {
+          if (e.target.nodeType === 3) {
+
+            var author = e.target.parentElement.previousElementSibling.innerText,
+              parent = e.target.parentElement.parentElement,
+              menu = parent.nextElementSibling;
+            //console.log(author,e.target.parentElement.innerText);
+            menu.addEventListener('click', function(f) {
+              //console.log(author);
+              //console.dir(blockbutton);
+              blockbutton = frame.querySelector('ytd-menu-service-item-renderer');
+              if (blockbutton) {
+                blockbutton.author = author;
+              }
+              var bbf = function(g) {
+                console.log(blockbutton.author);
+                if (!names.some(function(f) {
+                    return f === blockbutton.author;
+                  })) {
+                  names.push(blockbutton.author);
+                  chrome.storage.sync.set({
+                    "names": names
+                  });
+                }
+
+                blockbutton.removeEventListener('click', bbf);
+              };
+              blockbutton.addEventListener('click', bbf);
+
+            });
+            if (author && names.some(function(f) {
+                return f === author;
+              })) {
+              console.log("blocked " + author);
+              parent.parentElement.style.display = 'none';
             }
-        }
-    };
-    addEventListener('DOMNodeInserted', listenForAllComments);
-        chrome.storage.onChanged.addListener(function(changes, area) {
-        if (area === "sync") {
-            names = changes.names.newValue;
-        }
-    });
+            //console.dir(e);
+          }
+        });
+        removeEventListener("DOMNodeInserted", listenForAllComments);
+      }
+    }
+  };
+  addEventListener('DOMNodeInserted', listenForAllComments);
+  chrome.storage.onChanged.addListener(function(changes, area) {
+    if (area === "sync") {
+      names = changes.names.newValue;
+    }
+  });
 })();
